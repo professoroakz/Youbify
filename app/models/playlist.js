9e1f8cb3ea1d22df3239b5e0ee6d88b5635df23c
Oktay Gardener <oktayb@kth.se>
2015-03-01 14:53:31 +0100

    adds model for playlist

---
+++ app/models/playlist.js
@@ -0,0 +1,15 @@
+// grab the mongoose module
+var mongoose = require('mongoose');
+// create a new schema
+var Schema = mongoose.Schema;
+
+// define the schema
+var playlistSchema = new Schema({
+	name : { type: String, required: true },
+	id 	 : { type: int, required: true}
+	// Songschema here? Need songSchema objects
+});
+
+// define our model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('Playlist', playlistSchema);
