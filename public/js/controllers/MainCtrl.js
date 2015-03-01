781f05fa81191d34b657e139764729aebacc48a5
Oktay Gardener <oktayb@kth.se>
2015-03-01 21:58:25 +0100

    fixes startpage

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -2,6 +2,6 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
 	$scope.header = "Welcome to Youbify!"
     $scope.tagline = 'Tired of not finding your songs on Spotify?';
-    $scope.box = 'Youbify enables you to create playlists and add songs from youtube for instant playback!';
+    $scope.smalltext = 'Youbify enables you to create playlists and add songs from youtube for instant playback!';
 
 });
