bbfb8a2750d7257afb75cdbfafc24c60f4ef9659
oalsing <oscar.alsing@gmail.com>
2015-03-01 13:34:42 +0100

    REST for songs now works

--- app/routes.js
+++ app/routes.js
@@ -33,6 +33,38 @@ module.exports = function(app) {
         });
     });
 
+    app.put('/api/songs/:song_id', function(req, res) {
+        // use mongoose to get all songs in the database
+        Song.findById(req.params.song_id, function(err, song) {
+            if(err)
+                res.send(err);
+
+            song.name = req.body.name;
+            song.url = req.body.url;
+
+            // if there is an error retrieving, send the error. 
+            // nothing after res.send(err) will execute
+            song.save(function(err) {
+                if (err)
+                    res.send(err);
+
+            res.json(song); // return all songs in JSON format
+            });
+        });
+    });
+
+    app.delete('/api/songs/:song_id', function(req, res) {
+        // use mongoose to get all songs in the database
+        Song.remove({_id: req.params.song_id
+        }, function(err, song) {
+            if (err)
+                res.send(err);
+
+            res.json({message: "succesfully deleted"}); // return all songs in JSON format
+
+        });
+    });
+
     // route to handle creating goes here (app.post)
     app.post("/api/songs", function(req, res){
         var song = new Song();
