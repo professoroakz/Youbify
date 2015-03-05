b59d49073e31189d44062c58c4d84be5b15d4acf
Oktay Gardener <oktayb@kth.se>
2015-03-05 18:25:32 +0100

    adding songs to server fully working via view, methods in ctrl working

--- public/js/controllers/SongCtrl.js
+++ public/js/controllers/SongCtrl.js
@@ -3,6 +3,7 @@ angular.module('SongCtrl', []).controller('SongController', function($scope, $ht
     $scope.tagline = 'List of songs';
     $scope.addSongTagline = 'Add new songs!';
     $scope.whatPlaylist = 'Do you want to add the song to a playlist?';
+    $scope.songAdded = '';
     $http.get('/api/songs')
                 .success(function(data) {
                         $scope.songs = data;
@@ -11,13 +12,26 @@ angular.module('SongCtrl', []).controller('SongController', function($scope, $ht
                         console.log('Error: ' + data);
                 });
     $scope.addsong = function() {
-        /* while compiling form , angular created this object*/
-        var data=$scope.fields;  
-        /* post to server*/
-        $http.post(url, data);        
+         $http({
+	 	    url: '/api/songs',
+	 	    method: 'POST',
+	 	    headers: { 'Content-Type': 'application/json' },
+	 	    data: {
+	 	    	artist: $scope.inputArtist, 
+	 	    	title: $scope.inputTitle,
+	 	    	genre: $scope.inputGenre, 
+	 	    	url: $scope.inputYoutubeUrl}
+	 });
+	/*
+	*		Inform user of song added and clear the fields
+	*/
+        $scope.songAdded = 'Song successfully added!';
+        $scope.inputArtist = "";
+        $scope.inputTitle  = "";
+		$scope.inputGenre = "";
+		$scope.inputYoutubeUrl = "";
     }
 });
-
     /*
 
   var data = "{"_id":"54f4f8a991abf7ff3c64eef0","songz":[],"__v":1,"playlistsongs":[{"_id":"54f458dc6ded49621c043e54","url":"https://www.youtube.com/watch?v=FWR9MR5juWI","genre":"Deep House","title":"You Know You Like It","artist":"AlunaGeorge","__v":0},{"_id":"54f6ddaedc61acb63b406711","url":"https://www.youtube.com/watch?v=UtF6Jej8yb4","genre":"House","title":"The nights","artist":"Avicii","__v":0}]}";
