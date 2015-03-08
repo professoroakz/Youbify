49c2b6e7a55762c9fcbaa2bfc684d0bde9c0cb60
oalsing <oscar.alsing@gmail.com>
2015-03-08 17:23:44 +0100

    asd

--- public/js/controllers/SongDetailCtrl.js
+++ public/js/controllers/SongDetailCtrl.js
@@ -5,12 +5,11 @@ angular.module('SongDetailCtrl', []).controller('SongDetailController', function
 	var idmatch = url.match(idregex);
 	var id = idmatch[1];
 
-    $scope.tagline = 'Here\'s yo mad song mon!';
     $http.get('/api/songs/'+id)
                 .success(function(data) {
-                		console.log(data);
                 		$scope.title = data.title;
                 		$scope.artist = data.artist;
+                		$scope.genre = data.genre;
                 		$scope.url = $sce.trustAsResourceUrl(data.url);
                 })
                 .error(function(data) {
