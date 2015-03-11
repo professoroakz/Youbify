149aedc367c9d58d1ecaa4d8d1b5f2b3f39eb6e2
oalsing <oscar.alsing@gmail.com>
2015-03-11 14:28:15 +0100

    smart table now works for playlists

--- public/js/controllers/PlaylistCtrl.js
+++ public/js/controllers/PlaylistCtrl.js
@@ -1,4 +1,4 @@
-angular.module('PlaylistCtrl', []).controller('PlaylistController', function($scope, $http) {
+angular.module('PlaylistCtrl', ['smart-table']).controller('PlaylistController', function($scope, $http, $location) {
 
     $scope.tagline = 'Here\'s yo mad playlists mon!';
     $scope.createPlaylistTagLine = 'Create a new playlist';
@@ -41,4 +41,40 @@ angular.module('PlaylistCtrl', []).controller('PlaylistController', function($sc
         console.log('Error: ' + data);
     });
 }
+
+      var updateSongs = function(){
+    $http.get('/api/playlists')
+    .success(function(data) {
+      $scope.rowCollection = data;
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+
+    $scope.displayedCollection = [].concat($scope.rowCollection);
+  };
+
+  updateSongs();
+
+
+  $scope.removeItem = function removeItem(row) {
+    console.log(row);
+    var index = $scope.rowCollection.indexOf(row);
+    if (index !== -1) {
+      $scope.rowCollection.splice(index, 1);
+    }
+
+    $http.delete('/api/playlists/' + row._id)
+    .success(function(data) {
+      console.log("succesfully deleted" + row._id)
+
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+  }
+
+    $scope.go = function(row){
+    $location.path( "/playlists/" +row._id);
+  };
 });
