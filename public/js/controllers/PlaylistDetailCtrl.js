40309cb272ded45ce6b381025853e134b68d0eaf
oalsing <oscar.alsing@gmail.com>
2015-03-11 14:39:36 +0100

    smart table in playlist detail

--- public/js/controllers/PlaylistDetailCtrl.js
+++ public/js/controllers/PlaylistDetailCtrl.js
@@ -1,4 +1,4 @@
-angular.module('PlaylistDetailCtrl', []).controller('PlaylistDetailController', function($scope, $http, $location) {
+angular.module('PlaylistDetailCtrl', ['smart-table']).controller('PlaylistDetailController', function($scope, $http, $location) {
 	// /playlists/:id
 	var idregex = /playlists\/([^\/]+)/;
 	var url = $location.url();
@@ -6,11 +6,56 @@ angular.module('PlaylistDetailCtrl', []).controller('PlaylistDetailController', 
 	var id = idmatch[1];
 
     $scope.tagline = 'Here\'s yo mad playlists mon!';
-    $http.get('/api/playlists/'+id)
+ /*   $http.get('/api/playlists/'+id)
                 .success(function(data) {
                         $scope.playlist = data;
                 })
                 .error(function(data) {
                         console.log('Error: ' + data);
-                });
+                }); */
+
+    var updateSongs = function(){
+    $http.get('/api/playlists/'+id)
+    .success(function(data) {
+        console.log(data);
+      $scope.rowCollection = data.playlistsongs;
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+
+    $scope.displayedCollection = [].concat($scope.rowCollection);
+  };
+
+  updateSongs();
+
+
+  $scope.removeItem = function removeItem(row) {
+    var index = $scope.rowCollection.indexOf(row);
+    console.log(row);
+    if (index !== -1) {
+      $scope.rowCollection.splice(index, 1);
+    }
+
+    $http.delete('/api/songs/' + row._id)
+    .success(function(data) {
+      console.log("succesfully deleted" + row._id)
+
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+  }
+
+  $scope.go = function(row){
+    $location.path( "/songs/" +row._id);
+  };
+
+  $scope.play = function(row){
+    console.log(row);
+    var index = $scope.rowCollection.indexOf(row);
+    console.log(index);
+    $scope.rowCollection[index].video = "asd";
+    console.log($scope.rowCollection[index].video);
+  };
 });
