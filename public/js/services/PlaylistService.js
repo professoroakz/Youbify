034309302058a0cb7121ad070c33195c3542dc7c
Oktay Gardener <oktayb@kth.se>
2015-03-11 16:48:06 +0100

    main fixes

--- public/js/services/PlaylistService.js
+++ public/js/services/PlaylistService.js
@@ -11,26 +11,10 @@ var getPlaylist = function() {
     })
 }
 
-
     return {
         // call to get all playlists
         getPlaylist : getPlaylist
     }
 
-        // post : function() {
-        //     return $http.post('/api/playlists');
-        // },
-
-        // // these will work when more API routes are defined on the Node side of things
-        // // call to POST and create a new playlist
-        // create : function(playlistData) {
-        //     return $http.post('/api/playlists', playlistData);
-        // },
-
-        // // call to DELETE a playlist
-        // delete : function(id) {
-        //     return $http.delete('/api/playlists/' + id);
-        // }
-    
 
 }]);
