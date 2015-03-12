eea52b6016239b27779cc74d4f79749f7f2913bf
Oktay Gardener <oktayb@kth.se>
2015-03-12 22:32:57 +0100

    final changes on regex for youtube url

--- app/models/login.js
+++ app/models/login.js
@@ -8,5 +8,5 @@ var mongoose = require('mongoose');
 // define our model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Login', {
-    name : {type : String, default: ''}
+	name : {type : String, default: ''}
 });
