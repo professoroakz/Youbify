91938f7654071e3cafb4b0cd21af2da592db259a
oalsing <oscar.alsing@gmail.com>
2015-03-12 22:42:34 +0100

    update listeners

--- public/js/controllers/SongCtrl.js
+++ public/js/controllers/SongCtrl.js
@@ -136,7 +136,8 @@ $scope.addSongToPlaylist = function() {
       playlistsongs : $scope.playlistsongs
     }
   });
-
+  
+  updateSongs();
   $scope.songToPlaylistAdded = "Song added to playlist!";
   $scope.addSongToPlaylistVisible = true;
   $scope.addNewSongToPlaylistVisible = false;
